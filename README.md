68377dcdc4cab7d2a6651370cd99ce9a7e515adb
ismail.muhamad <ismail.muhamad@btpnsyariah.com>
2018-03-24 22:19:43 +0700

    first commit, add export_slik

---
+++ README.md
@@ -0,0 +1,17 @@
+# 3R
+enterprice report
+
+Prerequisite : 
+
+1.Pentaho pdi-ce-8.0.0.0-28
+
+folder :
+
+1.export_slik
+	berisikan program untuk export slik dari tabel ke csv
+	running :
+		windows :
+			run_export_slik.bat 			--> menjalankan untuk bulan kemarin
+			run_export_slik.bat 2017-12-31 	--> menjalankan untuk spesifik tanggal 2017-12
+		linux :
+			under development, menunggu install vmware redhat, dan di uji di lingkungan tersebut
---
+++ export_slik/run_export_slik.bat
@@ -0,0 +1,43 @@
+@echo off
+CLS
+TITLE "run_export_slik"
+
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+REM INIT
+
+SET "PENTAHO_PATH=C:\12023227\Tools\Program\pdi-ce-8.0.0.0-28\data-integration"
+SET "LOG_PATH=C:\12023227\Tools\Program\3R\PROGRAM\pdi\export\LOG"
+SET "full_wmic=C:\Windows\System32\wbem\WMIC.EXE"
+
+REM INIT DATE
+:: Check WMIC is available
+"%full_wmic%" Alias /? >NUL 2>&1 || GOTO gagal
+
+:: Use WMIC to retrieve date and time
+FOR /F "skip=1 tokens=1-6" %%G IN ('WMIC Path Win32_LocalTime Get Day^,Hour^,Minute^,Month^,Second^,Year /Format:table') DO (
+   IF "%%~L"=="" goto s_done
+      SET _yyyy=%%L
+      SET _mm=00%%J
+      SET _dd=00%%G
+      SET _hour=00%%H
+      SET _minute=00%%I
+	  SET _second=00%%K
+)
+
+:s_done
+
+:: Pad digits with leading zeros
+      SET _mm=%_mm:~-2%
+      SET _dd=%_dd:~-2%
+      SET _hour=%_hour:~-2%
+      SET _minute=%_minute:~-2%
+	  SET _second=%_second:~-2%
+
+:: Display the date/time in ISO 8601 format:
+SET tgl=%_yyyy%%_mm%%_dd%
+SET tglJam=%_yyyy%-%_mm%-%_dd% %_hour%:%_minute%:%_second%
+
+DEL %LOG_PATH%\export_slik_%tgl%.log
+
+%PENTAHO_PATH%\kitchen.bat /file=main_export_slik.kjb /logfile=%LOG_PATH%\export_slik_%tgl%.log /level=Detailed "/param:date_input=%1"
